a32466af6ec0cad4b82bc8be79a36a4cb60e7869
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-03-20 05:42:07 +0900

    ドロワー、モーダル制御の修正

--- src/js/script.js
+++ src/js/script.js
@@ -74,3 +74,86 @@ if (mvSplide) {
     arrows: false,
   }).mount();
 }
+
+/* ------------------------------
+YouTube再生モーダル
+------------------------------ */
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+
+  // p-movie-modalのclass名がつく要素を全取得
+  document.querySelectorAll(".p-movie-modal").forEach((imgTag) => {
+    imgTag.addEventListener("click", function () {
+      openModal(imgTag.getAttribute("data-video-id"));
+    });
+  });
+
+  // モーダルの要素を作成
+  let modal = document.createElement("div");
+  modal.id = "modal";
+  modal.className = "p-movie-modal__modal";
+
+  // モーダルの要素をクリックしたら、モーダルを閉じる
+  modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  // p-movie-modal__modal-contentの要素を作成
+  let modalContent = document.createElement("div");
+  modalContent.className = "p-movie-modal__modal-content";
+
+  // iframeのYouTube要素を作成
+  let videoPlayer = document.createElement("iframe");
+  videoPlayer.id = "videoPlayer";
+  videoPlayer.width = "560";
+  videoPlayer.height = "315";
+  videoPlayer.frameBorder = "0";
+  videoPlayer.allow =
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+  videoPlayer.setAttribute("allowfullscreen", "");
+
+  // 閉じるボタンを作成
+  let closeBtn = document.createElement("button");
+  closeBtn.className = "p-movie-modal__close";
+  closeBtn.addEventListener("click", closeModal);
+
+  // モーダルの要素を追加
+  modalContent.appendChild(videoPlayer);
+  modalContent.appendChild(closeBtn);
+  modal.appendChild(modalContent);
+  body.appendChild(modal);
+});
+
+// モーダル関数
+function openModal(videoId) {
+  let modal = document.getElementById("modal");
+  let videoPlayer = document.getElementById("videoPlayer");
+
+  if (!modal || !videoPlayer) return;
+
+  videoPlayer.src =
+    "https://www.youtube.com/embed/" + videoId + "?autoplay=1&rel=0";
+
+  modal.style.display = "flex";
+  modal.style.animation = "fadeIn 0.3s forwards";
+  document.body.style.overflow = "hidden";
+}
+
+// モーダルを閉じる関数を作成
+function closeModal() {
+  let modal = document.getElementById("modal");
+  let videoPlayer = document.getElementById("videoPlayer");
+
+  if (!modal || !videoPlayer) return;
+
+  modal.style.animation = "fadeOut 0.3s forwards";
+
+  setTimeout(() => {
+    videoPlayer.src = "";
+    modal.style.display = "none";
+  }, 700);
+
+  document.body.style.overflow = "visible";
+}
